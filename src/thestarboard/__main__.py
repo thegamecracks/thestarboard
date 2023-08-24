a81b3aa1a34672460737a4a9163aed149a72425e
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2023-08-23 22:40:50 -0400

    Use dpygt project as a template
    https://github.com/thegamecracks/discord.py-i18n-demo

---
+++ src/thestarboard/__main__.py
@@ -0,0 +1,51 @@
+import argparse
+import importlib.metadata
+import logging
+import sys
+
+import discord
+
+from .bot import Bot
+from .config import load_config
+
+parser = argparse.ArgumentParser(
+    prog=__package__,
+    description=importlib.metadata.metadata("thestarboard")["Summary"],
+)
+parser.add_argument(
+    "-v",
+    "--verbose",
+    action="count",
+    default=0,
+    help="Increase logging verbosity",
+)
+
+args = parser.parse_args()
+
+root_level = logging.INFO
+if args.verbose > 0:
+    log = logging.getLogger(__package__)
+    log.setLevel(logging.DEBUG)
+if args.verbose > 1:
+    root_level = logging.DEBUG
+
+# Configure logging early to capture our own initialization
+discord.utils.setup_logging(
+    level=root_level,
+    root=True,
+)
+
+config = load_config()
+
+if config.bot.token == "":
+    sys.exit(
+        "No bot token has been supplied by a config.toml file.\n"
+        "Please get a Bot Token from https://discord.com/developers/applications "
+        "and add it to your configuration."
+    )
+
+bot = Bot(config)
+bot.run(
+    config.bot.token,
+    log_handler=None,
+)
---
+++ src/thestarboard/bot.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+import importlib.metadata
+import logging
+from typing import TYPE_CHECKING
+
+from discord.ext import commands
+
+from .translator import GettextTranslator
+
+if TYPE_CHECKING:
+    from .config import Settings
+
+log = logging.getLogger(__name__)
+
+
+# https://discordpy.readthedocs.io/en/stable/ext/commands/api.html
+class Bot(commands.Bot):
+    def __init__(self, config: Settings):
+        super().__init__(
+            command_prefix=commands.when_mentioned,
+            intents=config.bot.intents.create_intents(),
+            strip_after_prefix=True,
+        )
+        self.config = config
+
+    async def _maybe_load_jishaku(self) -> None:
+        if not self.config.bot.allow_jishaku:
+            return
+
+        try:
+            version = importlib.metadata.version("jishaku")
+        except importlib.metadata.PackageNotFoundError:
+            pass
+        else:
+            await self.load_extension("jishaku")
+            log.info("Loaded jishaku extension (version: %s)", version)
+
+    async def setup_hook(self) -> None:
+        for path in self.config.bot.extensions:
+            await self.load_extension(path, package=__package__)
+        log.info("Loaded %d extensions", len(self.config.bot.extensions))
+        await self._maybe_load_jishaku()
+
+        await self.tree.set_translator(GettextTranslator())
+
+
+class Context(commands.Context[Bot]):
+    ...
